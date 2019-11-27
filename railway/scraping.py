c98ea5f5d1e0b322b552978b8abd978f5c91a11e
Jesse Oladimeji <Jesse@Jesses-MacBook-Air.local>
2019-11-27 14:34:16 +0000

    Station name & code scraping complete

--- railway/scraping.py
+++ railway/scraping.py
@@ -1,21 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 def get_railway_stations():
-  """Get a list of railway stations in Great Britain.
+    """Get a list of railway stations in Great Britain.
   
   Returns:
       {list} -- List of station names as dicts with their name and code.
   """
-  
-  stations = []  
-  URL = 'https://web.archive.org/web/20121125233554/http://www.nationalrail.co.uk/stations/codes/'
-  html = requests.get(URL).text
-  soup = BeautifulSoup(html, 'html.parser')
-  
-  print(soup.select_one('table'))
-  
-  return stations
 
+    stations = []
+    URL = "https://web.archive.org/web/20121125233554/http://www.nationalrail.co.uk/stations/codes/"
+    html = requests.get(URL).text
+    soup = BeautifulSoup(html, "html.parser")
 
-get_railway_stations()
+    # Select the names and codes
+    raw_names = soup.select("td.h, td.hospital")
+    raw_codes = soup.select("td.s, td.station")
+
+    # Format them nicely
+    names = [element.next_element.next_element for element in raw_names]
+    codes = [element.next_element for element in raw_codes]
+
+    # Return an array of dictionaries containing each station name and code
+    return [{"name": name, "code": codes[names.index(name)]} for name in names]
