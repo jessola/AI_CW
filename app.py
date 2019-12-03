088316d3b0e921c0cf382ab1eac3ad53a1a76c27
Jesse Ola (CMP - Student) <sgj17quu@UEA.AC.UK>
2019-12-03 13:20:47 +0000

    Minor changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-# from conversation import Conversation
 from conversation2 import Engine
+
 
 if __name__ == "__main__":
     # To start the conversation, type 'ticket' at the first prompt
@@ -8,11 +8,3 @@ if __name__ == "__main__":
     e.init_output_statement(lambda x: print(x))
     e.init_output_question(lambda x: e.listen(input(x)))
     e.run()
-    # c = Conversation()
-    # c.reset()
-    # c.run()
-
-    # while c.requires_more_info:
-    #     response = input(c.prompt_user() + "\n")
-
-    #     c.evaluate_response(response)
