1cdd94306cc28f5b6e1819b3dd5da3c855bbc6c1
Jesse Ola <Jesse@Jesses-MacBook-Pro.local>
2020-01-08 19:33:17 +0000

    Switched to a pusher backend

--- app.py
+++ app.py
@@ -1,32 +1,58 @@
-from flask import Flask
-from flask_socketio import SocketIO, send, emit
+from flask import Flask, request, jsonify
+import pusher
 
 from conversation2 import Engine
 from journeys import get_journeys, get_all_journeys, at_station
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
-io = SocketIO(app)
+# e = Engine()
+# e.reset()
+
+# Pusher Config
+pusher_client = pusher.Pusher(
+    app_id='928615',
+    key='99815334007378cc329d',
+    secret='07df68ecb505a9f822db',
+    cluster='eu',
+    ssl=True,
+)
 
 e = Engine()
 e.reset()
 
 
-def handle_statement(text):
-    # print(text)
-    emit('message', text)
+def output_statement(text):
+    pusher_client.trigger('my-channel', 'statement', {'message': text})
 
 
-@io.on('connect')
-def init_engine():
-    e.init_output_statement(handle_statement)
-    e.init_output_question(handle_statement)
-    e.run()
+def output_question(text):
+    pusher_client.trigger('my-channel', 'question', {'message': text})
 
 
-@io.on('received')
-def listen(data):
-    e.listen(data)
+e.init_output_statement(output_statement)
+e.init_output_question(output_question)
+e.run()
+
+# e = Engine()
+# e.reset()
+# e.init_output_statement(output_statement)
+# e.init_output_question(output_question)
+# e.run()
+
+
+@app.route('/', methods=['POST'])
+def user_message():
+    try:
+        message = request.json['data']
+        e.listen(message['text'])
+        # e.reset()
+        e.run()
+
+        return jsonify({'success': True}), 200
+    except:
+        print('FAILED')
+
+    return jsonify({'success': False}), 400
 
 
 # def test():
@@ -37,6 +63,9 @@ if __name__ == "__main__":
     # To start the conversation, type 'ticket' at the first prompt
     # e = Engine()
     # e.reset()
+    # e.init_output_statement(output_statement)
+    # e.init_output_question(output_question)
+    # e.run()
     # e.init_output_statement(lambda x: test(x))
     # e.init_output_question(lambda x: test(x))
     # e.init_output_statement(lambda x: print(x))
@@ -44,4 +73,5 @@ if __name__ == "__main__":
     # e.run()
 
     # at_station('DISS')
-    io.run(app)
+
+    app.run(debug=True)
