571f56cbad918e3cf5ed28efbc11ea8b09a1b28e
Joseph Comyn (CMP - Student) <kjd16veu@UEA.AC.UK>
2020-01-20 20:51:26 +0000

    message

--- NLP/POStest.py
+++ NLP/POStest.py
@@ -2,20 +2,20 @@ import spacy
 from datetime import datetime
 import re
 
-#nlp = spacy.load('en_core_web_lg')
+nlp = spacy.load('en_core_web_lg')
 
-#doc = nlp("I want a ticket from norwich to ely at 2:00pm on the 17th and return on the 18th and get there by 6:00pm ")
+doc = nlp("My train was delayed for 50 minutes can you tell me when ill reach my destination")
 
-#for ent in doc.ents:
-#    print(ent.text, ent.label_)
+for ent in doc.ents:
+    print(ent.text, ent.label_)
 
 #depdate = date.today()
 
-date = "the 17th"
-date = re.sub("[^0-9]", '', date)
-datenew = datetime.now()
-depdate = datenew.replace(day = int(date), hour = 0, minute = 0, second = 0).strftime('%d%m%y')
-print(depdate)
+#date = "the 17th"
+#date = re.sub("[^0-9]", '', date)
+#datenew = datetime.now()
+#depdate = datenew.replace(day = int(date), hour = 0, minute = 0, second = 0).strftime('%d%m%y')
+#print(depdate)
 
 
 
