59ba1dbb66dd38deb7990c425585f2f5c28c942c
Joseph Comyn (CMP - Student) <kjd16veu@UEA.AC.UK>
2020-01-22 12:36:06 +0000

    suhdfsinhefuo

--- NLP/NLP.py
+++ NLP/NLP.py
@@ -34,12 +34,16 @@ def inputNLP(input, returningInput = None):
             ticketdict.update({"returning": True})
 
 
-    #using entity 
+    #Loop through recognised entities, easier to pull important details from user text this way 
     for ent in doc.ents:
 
-        #departures
+        #If statement to handle all parts of user input related to Departure ticket queries
+        #Sorts it so entities before the word return are sorted as departure data
         if (returning == True and ent.start < tokenReturning) or (returningInput == True) or (returning != True and returningInput != True) :
+            #all location entities is sorted as to and from
             if ent.label_ == "GPE":
+                #to find the difference between going to location and leaving from location context must be found
+                #this is done by finding the preposition to the location and sorting it based on that 
                 if ent.start != 0:
                     prev_token = doc[ent.start - 1]
                     if prev_token.lemma_ in ("from", "depart"):
@@ -48,12 +52,15 @@ def inputNLP(input, returningInput = None):
                         ticketdict.update({"departing_to": ent.text})
 
             #find departure date
+            #can take multiple date formats such as 21/06/20 or 'the 16th'
             if ent.label_ == "DATE":
-                ticketdict.update({"departure_date": dateFormat(ent.text)})
+                ticketdict.update({"departure_date": dateFormat2(ent.text)})
 
             #find departure condition or time
             if ent.label_ == "TIME":
-                if ent.start != 0: 
+                #context is very important in deciding wether user wants to arrive at a time or leave the station at a certain time
+                if ent.start >= 2:
+                    #both previous words are taken 
                     prev_token1 = doc[ent.start - 1]
                     prev_token2 = doc[ent.start - 2]
                     if  prev_token1.dep == prep and prev_token2.lemma_ in ("arrive", "there"):
@@ -76,11 +83,11 @@ def inputNLP(input, returningInput = None):
 
             #find return date
             if ent.label_ == "DATE":
-                ticketdict.update({"return_date": dateFormat(ent.text)})
+                ticketdict.update({"return_date": dateFormat2(ent.text)})
 
             #find return condition or time
             if ent.label_ == "TIME":
-                if ent.start != 0: 
+                if ent.start >= 2: 
                     prev_token1 = doc[ent.start - 1]
                     prev_token2 = doc[ent.start - 2]
                     if  prev_token1.dep_ == "prep" and (prev_token2.lemma_ in ("arrive", "there")):
@@ -91,14 +98,14 @@ def inputNLP(input, returningInput = None):
                         ticketdict.update({"return_time": timeFormat(ent.text)})
 
     #//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    #experimental final pass to find possible named entities missed by large prediction model
+    #experimental final pass to find possible named entities missed by large prediction model as during testing some formats of date and time were not recognised
     # final pass to find potentially missed times in format hh:mm if there are missing departure date or time information
     if (ticketdict.get("departure_time") == None) or (ticketdict.get("departure_date") == None):
         for token in doc:
             #try to find missed out departure times
             if (returning == True and token.i < tokenReturning) or (returning != True and returningInput != True) :
                 if (re.search('\d{,2}:\d{,2}', token.text) is not None):
-                    if ent.start != 0:
+                    if ent.start >= 2:
                         prev_token1 = doc[ent.start - 1]
                         prev_token2 = doc[ent.start - 2]
                         if  prev_token1.dep == prep and prev_token2.lemma_ in ("arrive", "there"):
@@ -133,6 +140,7 @@ def inputNLP(input, returningInput = None):
 
     return ticketdict
 
+#takes user input for delay handling to find correct information
 def predictionNLP(input):
 
     doc = nlp(input)
@@ -166,6 +174,7 @@ def predictionNLP(input):
 
     return predictiondict
 
+#splits possible times imto either 12 hour or 24 hour clock for formatting
 def timeFormat(input):
     strtime = input
     if (re.search(r'\d{,2}\:\d{,2}\s?(?:AM|PM|am|pm)', input) is not None):
@@ -203,8 +212,13 @@ def dateFormat2(input):
         strarr = date.split("/")
         datetemp = datetime.now()
         datenew = datetemp.replace(day = int(strarr[0]), month = int(strarr[1]), hour = 0, minute = 0, second = 0).strftime('%d%m%y')
+    #For 00th date format
+    if (re.search(r'\d{,2}\w{,2}', date) is not None):
+        date = re.sub("[^0-9]", '', date)
+        datenow = datetime.now()
+        datenew = datenow.replace(day = int(date), hour = 0, minute = 0, second = 0).strftime('%d%m%y')
     return datenew
 
 
 
-print(inputNLP("I want a ticket on the 15/01/20 from Norwich to Cambridge arriving at 5:00pm and i want a return ticket for the 18/01/20 leaving at 14:00"))
+#print(inputNLP("I want a ticket on the 15th from Norwich to Cambridge arriving at 5:00pm and i want a return ticket for the 18th leaving at 14:00"))
